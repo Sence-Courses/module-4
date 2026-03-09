fd16b741cd6365d10acde375f85f7886fd7a55ee
JulianSence <55850745+JulianSence@users.noreply.github.com>
2026-03-09 20:00:46 -0300

    Revert "Enterprise view"

--- solutiontech/__main__.py
+++
@@ -1,8 +0,0 @@
-from views.main_view import TkinterApp
-from util import exec_query
-from util import EntQuery
-
-if __name__ == "__main__":
-  exec_query(EntQuery.create)
-  app = TkinterApp()
-  app.mainloop()
